02dc894ccd22b6970c474bd3b2f0d1dd0439fd9d
akansha-singh7 <81966555+akansha-singh7@users.noreply.github.com>
2021-09-04 23:05:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const ejs = require("ejs");
 const PORT = 3000;
 const app = express();
+
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
